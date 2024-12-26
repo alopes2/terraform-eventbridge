a0c37224e8b4ea61814600a30070e28f897d45b6
Andre <andrevitorlopes@gmail.com>
2024-12-26 18:18:38 +0100

    Add first rule

---
+++ iac/main.tf
@@ -0,0 +1,23 @@
+data "aws_cloudwatch_event_bus" "default" {
+  name = "default"
+}
+
+resource "aws_s3_bucket" "eventbridge" {
+  bucket = "eventbridge"
+}
+
+resource "aws_cloudwatch_event_rule" "s3_createobject" {
+  name           = "s3_createobject"
+  description    = "Rule to trigger when an object is created in the S3 bucket"
+  event_bus_name = data.aws_cloudwatch_event_bus.default.name
+
+  event_pattern = jsonencode({
+    source      = ["aws.s3"],
+    detail-type = ["Object Created"],
+    detail = {
+      bucket = {
+        name = ["${aws_s3_bucket.eventbridge.bucket}"]
+      }
+    }
+  })
+}
