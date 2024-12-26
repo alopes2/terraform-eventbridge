5b74fe9ca163c0c680f39c34c9460e5ecda49407
Andre <andrevitorlopes@gmail.com>
2024-12-26 21:03:52 +0100

    Add target to send to cloudwatch

--- iac/main.tf
+++ iac/main.tf
@@ -21,3 +21,59 @@ resource "aws_cloudwatch_event_rule" "s3_createobject" {
     }
   })
 }
+
+resource "aws_cloudwatch_event_target" "s3_createobject_log" {
+  rule      = aws_cloudwatch_event_rule.s3_createobject.name
+  target_id = "SendToCloudWatch"
+  arn       = aws_cloudwatch_log_group.s3_createobject.arn
+}
+
+resource "aws_cloudwatch_log_group" "s3_createobject" {
+  name              = "/aws/events/s3_createobject/logs"
+  retention_in_days = 1
+}
+
+data "aws_iam_policy_document" "s3_createobject_log_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream"
+    ]
+
+    resources = [
+      "${aws_cloudwatch_log_group.s3_createobject.arn}:*"
+    ]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "events.amazonaws.com",
+        "delivery.logs.amazonaws.com"
+      ]
+    }
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:PutLogEvents"
+    ]
+
+    resources = [
+      "${aws_cloudwatch_log_group.s3_createobject.arn}:*:*"
+    ]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "events.amazonaws.com",
+        "delivery.logs.amazonaws.com"
+      ]
+    }
+
+    condition {
+      test     = "ArnEquals"
+      values   = [aws_cloudwatch_event_rule.s3_createobject.arn]
+      variable = "aws:SourceArn"
+    }
+  }
+}
